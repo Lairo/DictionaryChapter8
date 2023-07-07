f81e5a6013082a69c12f9cb2637b2a29e1fb06f0
chajus1 <chajus1@googlemail.com>
2023-07-07 22:50:07 +0100

    plus FIFO LIFO

--- DictionaryChapter8/Program.cs
+++ DictionaryChapter8/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DictionaryChapter8
 {
@@ -10,24 +7,52 @@ namespace DictionaryChapter8
     {
         static void Main()
         {
-            Dictionary<int, RetiredPlayer> retiredYankees = new Dictionary<int, RetiredPlayer>()
-            {
-                {3, new RetiredPlayer("Babe Ruth", 1948)},
-                {4, new RetiredPlayer("Lou Gehrig", 1939)},
-                {5, new RetiredPlayer("Joe DiMaggio", 1952)},
-                {7, new RetiredPlayer("Mickey Mantle", 1969)},
-                {8, new RetiredPlayer("Yogi Berra", 1972)},
-                {10, new RetiredPlayer("Phil Rizzuto", 1985)},
-                {23, new RetiredPlayer("Don Mattingly", 1997)},
-                {42, new RetiredPlayer("Jackie Robinson", 1993)},
-                {44, new RetiredPlayer("Reggie Jackson", 1993)},
-            };
+            //Dictionary<int, RetiredPlayer> retiredYankees = new Dictionary<int, RetiredPlayer>()
+            //{
+            //    {3, new RetiredPlayer("Babe Ruth", 1948)},
+            //    {4, new RetiredPlayer("Lou Gehrig", 1939)},
+            //    {5, new RetiredPlayer("Joe DiMaggio", 1952)},
+            //    {7, new RetiredPlayer("Mickey Mantle", 1969)},
+            //    {8, new RetiredPlayer("Yogi Berra", 1972)},
+            //    {10, new RetiredPlayer("Phil Rizzuto", 1985)},
+            //    {23, new RetiredPlayer("Don Mattingly", 1997)},
+            //    {42, new RetiredPlayer("Jackie Robinson", 1993)},
+            //    {44, new RetiredPlayer("Reggie Jackson", 1993)},
+            //};
 
-            foreach (int jerseyNumber in retiredYankees.Keys)
-            {
-                RetiredPlayer player = retiredYankees[jerseyNumber];
-                Console.WriteLine($"{player.Name} #{jerseyNumber} retired in {player.YearRetired}");
-            }
+            //foreach (int jerseyNumber in retiredYankees.Keys)
+            //{
+            //    RetiredPlayer player = retiredYankees[jerseyNumber];
+            //    Console.WriteLine($"{player.Name} #{jerseyNumber} retired in {player.YearRetired}");
+            //}
+
+            //Queue<string> myQueue = new Queue<string>();
+            //myQueue.Enqueue("First in line");
+            //myQueue.Enqueue("second in line");
+            //myQueue.Enqueue("third in line");
+            //myQueue.Enqueue("last in line");
+
+            //Console.WriteLine($"Peek() returned:\n{myQueue.Peek()}");
+            //Console.WriteLine($"The first Dequeue(): returned:\n{myQueue.Dequeue()}");
+            //Console.WriteLine($"The second Dequeue(): returned:\n{myQueue.Dequeue()}");
+
+            //Console.WriteLine($"Count before Clear():\n{myQueue.Count}");
+            //myQueue.Clear();
+            //Console.WriteLine($"Count after Clear():\n{myQueue.Count}");
+
+            Stack<string> myStack = new Stack<string>();
+            myStack.Push("first in line");
+            myStack.Push("second in line");
+            myStack.Push("third in line");
+            myStack.Push("last in line");
+
+            Queue<string> myQueue = new Queue<string>(myStack);
+            List<string> myList = new List<string>(myQueue);
+            Stack<string> anotherStack = new Stack<string>(myList);
+
+            Console.WriteLine($@"myQueue has {myQueue.Count} items
+myList has {myList.Count} items
+anotherStack has {anotherStack.Count} items");
         }
     }
 }
